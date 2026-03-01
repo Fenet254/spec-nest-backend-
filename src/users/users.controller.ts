1cd5bab3c5979ffb9dd8e5011557ebcc4d0eb442
Fenet Gizaw <simbani011@gmail.com>
2026-03-01 04:34:11 -0800

    login and  register endpoints

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,19 +9,24 @@ import {
   ParseIntPipe,
   HttpCode,
   HttpStatus,
+  UseGuards,
+  Request,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { RolesGuard } from '../auth/guards/roles.guard';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Post()
-  @HttpCode(HttpStatus.CREATED)
-  async create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+  @Get('profile')
+  @UseGuards(AuthGuard('jwt'))
+  async getProfile(@Request() req) {
+    return this.usersService.findOne(req.user.id);
   }
 
   @Get()
@@ -44,6 +49,8 @@ export class UsersController {
 
   @Delete(':id')
   @HttpCode(HttpStatus.NO_CONTENT)
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
+  @Roles('admin')
   async remove(@Param('id', ParseIntPipe) id: number) {
     return this.usersService.remove(id);
   }
